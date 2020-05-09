11a4a02ad0ec3a6a16e3853b2e305bc761bb93e6
Austin Swartout <pictureofperception@gmail.com>
2020-05-09 18:35:09 -0400

    sumbit assignment 2

---
+++ basic_javascript_projects/JS/basic_javascript_2.js
@@ -0,0 +1,12 @@
+var total = 0;
+function myFunction() {
+    var add = 1;
+    document.getElementById("total").innerHTML = total = total + add;
+
+}
+
+function testFunction() {
+    var sentence = "this";
+    sentence += " and other things";
+    document.getElementById("con").innerHTML = sentence;
+}
